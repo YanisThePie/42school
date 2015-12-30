08b3ef84323f7b0143c0904c2d852ce519d5c3a5
Yanis ISMAIL <yismail@e1r7p13.42.fr>
2015-12-30 23:53:30 +0100

    11:53

--- fillit/create_chain.c
+++ fillit/create_chain.c
@@ -6,12 +6,12 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 20:19:48 by yismail           #+#    #+#             */
-/*   Updated: 2015/12/30 21:51:31 by yismail          ###   ########.fr       */
+/*   Updated: 2015/12/30 23:52:53 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 char **create_tab (char *buf)
 {
@@ -36,25 +36,24 @@ char **create_tab (char *buf)
 	  lin ++;
 	  i++;
 	}
-
-  tab = malloc(sizeof(char*) * lin);
-  if (tab == NULL)
-	return (0);
+  lin = (lin - 1);
+  if (!(tab = (char **)malloc(sizeof(char *) * 16)))
+	  return (0);
   j = 0;
-  while (j < col)
-	{
-	  tab[j] = malloc (sizeof(char) * col);
-		j++;
-	}
+  while (j < 5)
+  {
+	 tab[j] = (char *)malloc(sizeof(char) * 4);
+	 j++;
+  }
   
   lin = 0;
-  col = 0;
-  
-  while (i < 21)
+  i = 0;
+  while (lin < 4)
 	{
-	  f = 0;
-	  while (f < 5 && buf[i] != '\n')
+	  col = 0;
+	  while (col < 4 && buf[i] != '\n')
 		{
+		  //  printf("%c", buf[i]);
 		  tab[lin][col] = buf[i];
 		  col++;
 		  i++;
@@ -64,13 +63,61 @@ char **create_tab (char *buf)
 
 return (tab);
 }
+
+char **create(char *buf)
+{
+  char **tab;
+  int lin;
+  int col;
+  int i;
+
+  i = 0;
+  lin = 0;
+  col = 0;
+  if (!(tab = (char **)malloc(sizeof(char *) * 16)))
+ 	return (0);
+  while (col < 4)
+  {
+  	tab[col] = (char *)malloc(sizeof(char) * 1);
+	col++;
+  }
+  while (lin < 4)
+  {
+	   col = 0;
+	   while (col < 4)
+	   {
+		 tab[lin][col] = buf[i];
+		 col++;
+		 i++;
+	   }
+	   i++;
+	   lin++;
+  }
+  return(tab);
+}
+
+
 int main (void)
 {
   char *str;
   char **tab;
-  
-  str = "..#.\n..#.\n...#\n.#..\n\n";
-  tab = create_tab(str);
-  printf ("%s", tab[1]);
+  int i;
+  int j;
+
+
+  i = 0;
+  str = "..#.\n..#.\n...#\n.##.\n\n";
+  tab = create(str);
+  while (i < 4)
+  {
+	j = 0;
+	while (j < 4)
+	{
+	  printf("%c", tab[i][j]);
+	  j++;
+	}
+	printf("\n");
+	i++;
+  }
   return (0);
 }
