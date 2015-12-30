84e6cbe991e3780ea2e57f5eff1afccf07919b79
Yanis ISMAIL <yismail@e1r7p13.42.fr>
2015-12-30 21:52:23 +0100

    yo

--- fillit/create_chain.c
+++ fillit/create_chain.c
@@ -6,11 +6,14 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/30 20:19:48 by yismail           #+#    #+#             */
-/*   Updated: 2015/12/30 21:29:50 by yismail          ###   ########.fr       */
+/*   Updated: 2015/12/30 21:51:31 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
 
-int **create_tab (char *buf)
+
+char **create_tab (char *buf)
 {
   int col;
   int lin;
@@ -40,22 +43,34 @@ int **create_tab (char *buf)
   j = 0;
   while (j < col)
 	{
-	  tab[j] = malloc (sizeof(char) * col)
+	  tab[j] = malloc (sizeof(char) * col);
 		j++;
 	}
   
   lin = 0;
   col = 0;
- 
+  
   while (i < 21)
 	{
 	  f = 0;
-	  while (f < 5)
+	  while (f < 5 && buf[i] != '\n')
 		{
-		  tab[lin][col2] = buf[i];
+		  tab[lin][col] = buf[i];
 		  col++;
 		  i++;
 		}
-		lin++;
-		}
+	  lin++;
 	}
+
+return (tab);
+}
+int main (void)
+{
+  char *str;
+  char **tab;
+  
+  str = "..#.\n..#.\n...#\n.#..\n\n";
+  tab = create_tab(str);
+  printf ("%s", tab[1]);
+  return (0);
+}
