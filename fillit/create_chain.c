3b16ed8a71f3248a9a9a45686199ce2b1fd636ad
user <user@MacBook-Pro-de-user-3.local>
2015-12-31 16:55:36 +0100

    lst created

--- fillit/create_chain.c
+++ fillit/create_chain.c
@@ -13,104 +13,140 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include "../libft/libft.h"
+#include "fillit.h"
 
-/*
+
 int print_tab (char **tab)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (i < 4)
+  int i;
+  int j;
+  
+  i = 0;
+  while (i < 4)
+    {
+      j = 0;
+      while (j < 4)
 	{
-		j = 0;
-        while (j < 4)
-		{
-			printf("%c", tab[i][j]);
-			j++;
-		}
-		printf("\n");
-		i++;
+	  printf("%c", tab[i][j]);
+	  j++;
 	}
-	printf ("\n");
-	return (0);
+      printf("\n");
+      i++;
+    }
+  printf ("\n");
+  return (0);
 }
-*/
 
-void create_list (char **tab, t_list **alst)
+d_list *ft_dlstnew(void const *content, size_t content_size)
 {
-	t_list *new;
-		new = ft_lstnew(tab, (sizeof(char) * 21));
-		if(*alst == NULL)
-			*alst = new;
-		new->next = NULL;
+  d_list*new;
+
+  if ((new = (d_list*)malloc(sizeof(d_list))) == NULL)
+    return (NULL);
+  new->next = NULL;
+  if (content == NULL)
+    {
+      new->content = NULL;
+      new->content_size = 0;
+    }
+  else
+    {
+      new->content = malloc(content_size);
+      if (new->content == NULL)
+	{
+	  free(new);
+	  return (NULL);
+	}
+      ft_memcpy(new->content, content, content_size);
+      new->content_size = content_size;
+    }
+  return(new);
+}
+
+void create_list (char **tab)
+{
+  d_list *new;
+  d_list *temp;
+  d_list **alst;
+
+  new = NULL;
+  new = ft_dlstnew(tab, (sizeof(char) * 21));
+  new->content = tab;
+  new->next = NULL;
+  if(*alst == NULL)
+    *alst = new;
+  temp = *alst;
+  while (temp->next != NULL)
+    {
+      temp = temp->next;
+    }
+  temp->next = new;
+  print_tab ((char**)new->content);
 }
 int create_tab(char *piece)
 {
-	char **tab;
-	t_list **alst;
-	int lin;
-	int col;
-	int i;
+  char **tab;
+  int lin;
+  int col;
+  int i;
+  d_list **tempor;
 	
-	i = 0;
-	lin = 0;
-	col = 0;
-	if (!(tab = (char **)malloc(sizeof(char *) * 16)))
-		return (0);
-	while (col < 4)
-	{
-		if (!(tab[col] = (char *)malloc(sizeof(char) * 1)))
-			return (0);
-		col++;
-	}
-	while (lin < 4)
-	{
-		col = 0;
-		while (col < 4)
-		{
-			tab[lin][col] = piece[i];
-			col++;
-			i++;
-		}
-		i++;
-		lin++;
-	}
-	create_list(tab, alst);
-//	print_tab (tab);
+  i = 0;
+  lin = 0;
+  col = 0;
+  if (!(tab = (char **)malloc(sizeof(char *) * 16)))
+    return (0);
+  while (col < 4)
+    {
+      if (!(tab[col] = (char *)malloc(sizeof(char) * 1)))
 	return (0);
+      col++;
+    }
+  while (lin < 4)
+    {
+      col = 0;
+      while (col < 4)
+	{
+	  tab[lin][col] = piece[i];
+	  col++;
+	  i++;
+	}
+      i++;
+      lin++;
+    }
+  create_list(tab);
+  return (0);
 }
 
 void take (char *buf)
 {
-	int i;
-	int j;
-	char *tmp;
-	int n;
-	
-	i = 0;
-	j = 0;
-	n = 0;
-	while (buf[j] != '\0')
-	{
-		i++;
-		j++;
-		if (buf[j] == '\n' && buf[j + 1] == '\n')
-		{	
-			tmp = ft_strsub(buf, (j - i), (i + 2));
-			create_tab (tmp);
-			tmp = NULL;
-			i = 0;
-			j = j + 2;
-		}
+  int i;
+  int j;
+  char *tmp;
+  int n;
+  
+  i = 0;
+  j = 0;
+  n = 0;
+  while (buf[j] != '\0')
+    {
+      i++;
+      j++;
+      if (buf[j] == '\n' && buf[j + 1] == '\n')
+	{	
+	  tmp = ft_strsub(buf, (j - i), (i + 2));
+	  create_tab (tmp);
+	  tmp = NULL;
+	  i = 0;
+	  j = j + 2;
 	}
+    }
 }
 int main (void)
 {
-	char *str;
-
-	str = ft_strdup("..#.\n..#.\n...#\n...#\n\n..#.\n...#\n..#.\n..#.\n\n.#..\n#...\n.#..\n.#..\n\n");
-	take (str);
-	return (0);
+  char *str;
+  
+  str = ft_strdup("..#.\n..#.\n...#\n...#\n\n..#.\n...#\n..#.\n..#.\n\n.#..\n#...\n.#..\n.#..\n\n..#.\n.#..\n.#..\n#...\n\n");
+  take (str);
+  return (0);
 }
--- fillit/fillit.h
+++ fillit/fillit.h
@@ -18,11 +18,19 @@
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <unistd.h>
-# include "./libft/libft.h"
+# include "../libft/libft.h"
 # include <stdio.h>
 # define ERROR { printf("Error in %s, at line %d\n", __FILE__, __LINE__); exit(EXIT_FAILURE); }
 
-int			get_pieces(int fd);
+typedef struct	dlist
+{
+  void		*content;
+  size_t	content_size;
+  struct dlist	*prev;
+  struct dlist *next;
+}		d_list;
+
+int		get_pieces(int fd);
 void		verify_piece(char *buf);
 void		verify_n(char *buf);
 void		verify_link(char *buf);
