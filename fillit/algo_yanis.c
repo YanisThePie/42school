8f22feb25c7519b311b98bbecd23e99e4334ac55
user <user@MacBook-Pro-de-user-3.local>
2016-01-11 21:53:19 +0100

    algo en cours

--- fillit/algo_yanis.c
+++ fillit/algo_yanis.c
@@ -1,6 +1,3 @@
-
-
-
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -17,131 +14,86 @@
 
 void print_it (char **tab)
 {
-	int i;
+  int i;
 
-	i = 0;
-	ft_putstr (tab[i]);
-	i++;
-	ft_putstr (tab[i]);
-	i++;
-	ft_putstr (tab[i]);
+  i = 0;
+  ft_putstr (tab[i]);
+  i++;
+  ft_putstr (tab[i]);
+  i++;
+  ft_putstr (tab[i]);
 }
 
 char **malloc_square (size_t size)
 {
-	char **square;
-	size_t i;
-	size_t j;
-	i = 0;
-	j = 0;
-	square = (char**)malloc (sizeof(char*) * (size * size) + 1);
-	while (i <= size)
-	{	
-		square[i] = (char*)malloc(sizeof(char) * (size) + 1);
-		i++;
-	}
-	i = 0;
-	while (i < size)
+  char **square;
+  size_t i;
+  size_t j;
+  i = 0;
+  j = 0;
+  square = (char**)malloc (sizeof(char*) * (size * size) + 1);
+  while (i <= size)
+    {	
+      square[i] = (char*)malloc(sizeof(char) * (size) + 1);
+      i++;
+    }
+  i = 0;
+  while (i < size)
+    {
+      j = 0;
+      while (j < size)
 	{
-		j = 0;
-		while (j < size)
-		{
-			square[i][j] = '.';
-			j++;
-		}
-		square[i][j] = '\n';
-		i++;
+	  square[i][j] = '.';
+	  j++;
 	}
-	return (square);		
+      square[i][j] = '\n';
+      i++;
+    }
+  return (square);		
 }
-/*
-char **place_piece(char **square, t_dlist *list)
-{
-	int i;
-	int j;
-	int lin;
-	int col;
 
-	i = 0;
-	j = 0;
-	lin = 0;
-	col = 0;
-	while (list->content[i][j] != '\0')
-	{
-		ft_putnbr (i);
-		ft_putchar ('\n');
-		j = 0;
-		while(list->content[i][j] != '\n')
-		{
-			ft_putnbr (j);
-			ft_putchar ('\n');
-			if (list->content[i][j] == '#')
-			{
-				lin = 0;
-				while (square[lin][col] != '\0')
-				{
-					ft_putnbr (lin);
-					ft_putchar ('\n');
-					col = 0;
-					while (square[lin][col] != '\n')
-					{
-						ft_putnbr (col);
-						ft_putchar ('\n');
-						if (square [lin][col] == '.')
-							square[lin][col] = list->content[i][j];
-						col++;
-					}
-					lin++;
-				}
-			}
-			j++;
-		}
-		i++;
-	}
-	return (square);
-*/
-int is_right (char **square, int lin, int col, int size)
+int is_right (char **square, int lin, int col,int size)
 {
-	ft_putnbr (col);
-	ft_putchar('C');
-	ft_putchar ('\n');
-	ft_putnbr(lin);
-	ft_putchar('L');
-	ft_putchar ('\n');
-	if (square[lin][col] == '\0')
-		return (1);
-	//if (col == size)
-	//{
-		//ft_putnbr(lin);
-		//ft_putchar('L');
-		//ft_putchar ('\n');
-	//	lin = lin + 1;
+  if (n > 0)
+    return (0);
+  if (square[lin][col] == '\0')
+    return (1);
+  //if (col == size)
+  //{
+  //ft_putnbr(lin);
+  //ft_putchar('L');
+  //ft_putchar ('\n');
+  //	lin = lin + 1;
 	//	col = -1;
 	//}
-	if(square[lin][col] == '.' && (col < size))
-		return (is_right(square, lin, col+1, size));
-	else
+  if(square[lin][col] == '.' && (col < size))
+    {
+      if((verif_tetri(square[lin][col], list->content)) == 1)
+	n--;
+      return (is_right(square, lin, col+1, size));
+    }
+  else
+    {
+      if (lin < size)
 	{
-		if (lin < size)
-		{
-			col = -1;
-			return (is_right(square, lin + 1, col + 1, size));
-		}
+	  col = -1;
+	  return (is_right(square, lin + 1, col + 1, size));
 	}
-	return (0);
+    }
+  return (0);
 }
 
 int place_in_square (t_dlist *list)
 {
-	char **square;
-	int size;
-	int result;
-
-	list = NULL;//a retirer
-	size = 2;
-	square = malloc_square(size);
-	result = is_right(square, 0, 0, size);
-	ft_putnbr (result);
-	print_it (square);
-	return (0);
+  char **square;
+  int size;
+  int result;
+  
+  list = NULL;//a retirer
+  size = 2;
+  square = malloc_square(size);
+  result = is_right(square, 0, 0, size);
+  //ft_putnbr (result);
+  print_it (square);
+  return (0);
 }
