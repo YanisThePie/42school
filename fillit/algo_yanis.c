32da5bffa641e5954ef78a8c9ac206b5fa3ce1c3
user <user@MacBook-Pro-de-user-3.local>
2016-01-12 07:28:11 +0100

    emacs config

--- fillit/algo_yanis.c
+++ fillit/algo_yanis.c
@@ -14,86 +14,86 @@
 
 void print_it (char **tab)
 {
-  int i;
-
-  i = 0;
-  ft_putstr (tab[i]);
-  i++;
-  ft_putstr (tab[i]);
-  i++;
-  ft_putstr (tab[i]);
+	int i;
+	
+	i = 0;
+	ft_putstr (tab[i]);
+	i++;
+	ft_putstr (tab[i]);
+	i++;
+	ft_putstr (tab[i]);
 }
 
 char **malloc_square (size_t size)
 {
-  char **square;
-  size_t i;
-  size_t j;
-  i = 0;
-  j = 0;
-  square = (char**)malloc (sizeof(char*) * (size * size) + 1);
-  while (i <= size)
-    {	
-      square[i] = (char*)malloc(sizeof(char) * (size) + 1);
-      i++;
+	char **square;
+	size_t i;
+	size_t j;
+	i = 0;
+	j = 0;
+	square = (char**)malloc (sizeof(char*) * (size * size) + 1);
+	while (i <= size)
+	{	
+		square[i] = (char*)malloc(sizeof(char) * (size) + 1);
+		i++;
     }
-  i = 0;
-  while (i < size)
+	i = 0;
+	while (i < size)
     {
-      j = 0;
-      while (j < size)
-	{
-	  square[i][j] = '.';
-	  j++;
-	}
+		j = 0;
+		while (j < size)
+		{
+			square[i][j] = '.';
+			j++;
+		}
       square[i][j] = '\n';
       i++;
     }
-  return (square);		
+	return (square);		
 }
 
 int is_right (char **square, int lin, int col,int size)
 {
   if (n > 0)
-    return (0);
+	  return (0);
   if (square[lin][col] == '\0')
-    return (1);
+	  return (1);
   //if (col == size)
   //{
   //ft_putnbr(lin);
   //ft_putchar('L');
   //ft_putchar ('\n');
   //	lin = lin + 1;
-	//	col = -1;
-	//}
+  //	col = -1;
+  //}
   if(square[lin][col] == '.' && (col < size))
-    {
+  {
       if((verif_tetri(square[lin][col], list->content)) == 1)
-	n--;
+		  n--;
       return (is_right(square, lin, col+1, size));
-    }
+  }
   else
-    {
+  {
       if (lin < size)
-	{
-	  col = -1;
-	  return (is_right(square, lin + 1, col + 1, size));
-	}
-    }
+	  {
+		  col = -1;
+		  return (is_right(square, lin + 1, col + 1, size));
+	  }
+  }
   return (0);
 }
 
 int place_in_square (t_dlist *list)
 {
-  char **square;
-  int size;
-  int result;
-  
-  list = NULL;//a retirer
-  size = 2;
-  square = malloc_square(size);
-  result = is_right(square, 0, 0, size);
-  //ft_putnbr (result);
-  print_it (square);
-  return (0);
+	char **square;
+	int size;
+	int result;
+	
+	list = NULL;//a retirer
+	size = 2;
+	square = malloc_square(size);
+	result = is_right(square, 0, 0, size);
+	//ft_putnbr (result);
+	print_it (square);
+	return (0);
 }
