7511257da9d47b0715336d2728847bac8eec09e5
Yanis ISMAIL <yismail@e1r4p14.42.fr>
2016-02-03 18:59:03 +0100

    did

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,11 +6,11 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 13:24:01 by yismail           #+#    #+#             */
-/*   Updated: 2016/02/03 16:32:31 by yismail          ###   ########.fr       */
+/*   Updated: 2016/02/03 18:58:11 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFF_SIZE  8 
+#define BUFF_SIZE 75
 #include "get_next_line.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -26,7 +26,7 @@ char *keep_str_tmp (char *str, char *str2)
 	j = 0;
 	while (str && j < BUFF_SIZE)
 	{
-		while (str[i] == str2[i])
+		while (str[i] == str2[i] && str && str2)
 			i++;
 		j++;
 	}
@@ -47,7 +47,7 @@ char * check_for_new_line (char *str_buf, int *j)
 		i++;
 	if (str_buf[i] == '\n')
 	{
-		str_tmp = malloc (sizeof(char) * i);
+		str_tmp = malloc (sizeof(char) * (i + 1));
 		ft_strncpy(str_tmp, str_buf, i);
 		str_tmp[i] = '\0';
 		while (i < BUFF_SIZE)
@@ -55,7 +55,6 @@ char * check_for_new_line (char *str_buf, int *j)
 			i++;
 			cpt++;
 		}
-		ft_putstr(str_buf);
 		*j = (i - cpt);
 	}
 	return (str_tmp);
@@ -71,13 +70,13 @@ int get_the_buffer(int const fd, char **line, char **membuffer_true, int *j)
   
   i = *j;
   newline = NULL;
-  membuffer = ft_strnew(BUFF_SIZE);
-  while ((r = read (fd, buf, BUFF_SIZE)))
+  membuffer = ft_strnew(0);
+  while ((r = read (fd, buf, BUFF_SIZE)) > 0)
   {
-	  if (r == -1)
-		  return (-1);
 	  buf[r] = '\0';
-	  membuffer = ft_strjoin (membuffer, buf);
+	  membuffer = ft_strjoin(membuffer, buf);
+	  // ft_strjoinfree? ne pas oubier de free membuffer avant
+	  //d'ecraser;
 	  newline = check_for_new_line(membuffer, &i);
 	  if (  newline != NULL)
 	  {
@@ -87,7 +86,7 @@ int get_the_buffer(int const fd, char **line, char **membuffer_true, int *j)
 		  return(1);
 	  }
   }
-  return (0);
+  return (r);
 }
 int get_next_line (int const fd, char **line)
 {
@@ -97,11 +96,13 @@ int get_next_line (int const fd, char **line)
 	int i;
 
 	i = 0;
-	membuffer = ft_strnew(1);
-	*line = ft_strnew(1);
+	membuffer = ft_strnew(0);
+	*line = ft_strnew(0);
 	ret = get_the_buffer(fd, line, &membuffer, &i);
 	if (str_to_keep)
 	{
+		//verifier si \n dans str_to_keep au deuxieme passage,
+		//gerer le deuxieme passage absolument.
 		*line = ft_strcat (str_to_keep, *line);
 		str_to_keep = NULL;
 	}
@@ -120,10 +121,15 @@ int main (int argc, char **argv)
 	send = NULL;
 	fd = open(argv[1], O_RDONLY);
 	get_next_line(fd, &send);
-	//printf("%s",send);
+	ft_putstr(send);
+	ft_putstr("\nTEST1\n");
 	get_next_line(fd, &send);
-	//printf("%s",send);
+    ft_putstr(send);
+	ft_putstr("\nTEST2\n");
 	get_next_line(fd, &send);
-	//printf("%s",send);
+    ft_putstr(send);
+	ft_putstr("\nTEST3\n");
+	get_next_line(fd, &send);
+    ft_putstr(send);
 	return (0);
 }
