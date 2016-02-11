c58e276b2ec27be76cd63b76e364b2072e307660
Yanis ISMAIL <yismail@e1r11p3.42.fr>
2016-02-11 18:01:56 +0100

    get next line presque fini

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,28 +6,17 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 17:56:55 by yismail           #+#    #+#             */
-/*   Updated: 2016/02/10 21:50:11 by yismail          ###   ########.fr       */
+/*   Updated: 2016/02/11 18:01:12 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *what_is_more(char *str, char* str2)
+int check_fd(int fd)
 {
-	int i;
-	int j;
-	char* str_to_send;
-
-	i = 0;
-	j = 0;
-	while (str[i] == str2[i])
-		i++;
-	while (str)
-		j++;
-	str_to_send = (char*) malloc (sizeof(char) * j);
-	ft_bzero (str_to_send, j);
-	str_to_send = ft_strsub(str, j - i, j);
-	return (str_to_send);
+	if (fd < 0)
+		return (-1);
+	return (0);
 }
 
 int how_long_for_n (char* str)
@@ -39,49 +28,60 @@ int how_long_for_n (char* str)
 		i++;
 	return (i);
 }
-
-
 int get_next_line (int const fd, char **line)
 {
 	int ret;
 	int size;
 	char *buf;
 	char *newline;
-	static char *str_to_keep = 0;
+	static char *str_to_keep;
 
+	if (check_fd(fd) == -1)
+		return (-1);
 	newline = ft_strnew(0);
 	buf = ft_strnew(BUFF_SIZE);
 	size = 0;
-
+	if (str_to_keep)	
+	{
+		*line = ft_strnew(0);
+		if (ft_strchr(str_to_keep, '\n'))
+		{
+			size = how_long_for_n (str_to_keep);
+			*line = ft_strdup(ft_strjoin(*line, ft_strsub(str_to_keep, 0, size)));
+			str_to_keep = ft_strdup(ft_strchr(str_to_keep, '\n') + 1);
+			return (1);
+		}
+		else
+			*line = ft_strdup(str_to_keep);
+	}
+	else
+		str_to_keep = ft_strnew(BUFF_SIZE);
 	while ((ret = (read (fd, buf, BUFF_SIZE) > 0)))
 	{
 		if (ft_strchr(buf, '\n'))
 		{
-			ft_putstr(*line);
 			size = how_long_for_n (buf);
-			*line = ft_strjoin(*line, ft_strsub(buf, 0, size));
-			str_to_keep = (ft_strchr(buf, '\n') + 1);
+			if (*line)
+				*line = ft_strjoin_free(*line, ft_strsub(buf, 0, size));
+			else 
+				*line = ft_strsub(buf, 0, size);
+			str_to_keep = ft_strdup(ft_strchr(buf, '\n') + 1);
 			free(buf);
 			return (ret);
 		}
 		else
 		{
+			if (*line == NULL)
+				*line = ft_strnew(1);
 			if (*line != NULL)
-				*line = ft_strjoin(*line, buf);
-			else
-			{
-				if (str_to_keep)
-					*line = ft_strjoin (str_to_keep, buf);
-				else
-					*line = buf;
-			}
+				*line = ft_strjoin_free(*line, buf);
 		}
 		free(buf);
 		buf = ft_strnew(BUFF_SIZE + 1);
 	}
 	return (ret);
 }
-
+/*
 int main (int argc, char **argv)
 {
     char *send;
@@ -94,9 +94,17 @@ int main (int argc, char **argv)
     send = NULL;
     fd = open(argv[1], O_RDONLY);
 
-    get_next_line(fd, &send);
-	//ft_putstr(send);
-    get_next_line(fd, &send);
-    //ft_putstr(send);
+    while(get_next_line(fd, &send) > 0)
+	{
+		if (send != NULL)
+		{	
+			ft_putendl(send);
+			free(send);
+			send = NULL;
+		}
+	}
+//	get_next_line(fd, &send);
+	//   ft_putendl(send);
 	return (0);
 }
+*/
