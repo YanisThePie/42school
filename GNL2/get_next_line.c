e62aeacf7fec486c5225e52200ff565793ab0ef0
Yanis ISMAIL <yismail@e1r10p1.42.fr>
2016-03-07 14:12:52 +0100

    GNL2

--- GNL2/get_next_line.c
+++ GNL2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/06 18:00:22 by yismail           #+#    #+#             */
-/*   Updated: 2016/03/07 12:16:25 by yismail          ###   ########.fr       */
+/*   Updated: 2016/03/07 14:10:25 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,16 @@ int     how_long_for(char *str, char c)
     return (i);
 }
 
-int str_to_keep ()
+char *str_to_keep (char *after_n)
 {
-	const char *str_to_keep;
+	static char *str_to_keep;
 	char *to_send;
+	char *to_keep;
 	int size;
 
 	if (str_to_keep)
 	{
+		str_to_keep = ft_strjoin_free (str_to_keep, after_n);
 		if (ft_strchr (str_to_keep, '\n'))
 		{
 			size = how_long_for (str_to_keep, '\n');
@@ -38,37 +40,50 @@ int str_to_keep ()
 			return (to_send);
 		}
 		else
-			return (str_to_keep);
+		{
+			to_keep = ft_strdup(str_to_keep);
+			ft_bzero(str_to_keep, ft_strlen(str_to_keep)); // moyen de ne pas ecrire par dessus sans tenter de l'effacer?
+			return (str_to_keep); //else return all.
+		}
 	}
+	str_to_keep = ft_strnew (0);
+	str_to_keep = ft_strdup (after_n);
+	return (str_to_keep);
 }
 
-char *read_the_buffer(int fd)
+char *read_the_buffer(int fd, char **after_n)
 {
 	char *buf;
 	char *buf_joined;
 
-	*buf = ft_strnew(BUFF_SIZE);
+	buf = ft_strnew(BUFF_SIZE);
 	while (read(fd, buf, BUFF_SIZE) > 0)
 	{
-		if (ft_strchr(*buf, '\n'))// if we found a \n, stop the read.
-			return (*buf);
+		if (ft_strchr(buf, '\n'))// if we found a \n, send all the rest to gnl and stop the read.
+		{
+			*after_n = ft_strdup(strchr(buf, '\n') + 1);
+			return (buf);
+		}
 		else
 		{
 			buf_joined = (buf_joined == NULL) ? ft_strnew(0) : buf_joined;
-			buf_joined = ft_strjoin_free(*buf_joined, *buf);// we join buffers, or we create one if doesnt exist.
+			buf_joined = ft_strjoin(buf_joined, buf);// we join buffers, or we create one if doesnt exist.
 		}
 	}
-	return (*buf);// If Read is over, return the buffer.
+	return (buf);// If Read is over, return the buffer.
 }
 int get_next_line(int const fd, char **line)
 {
-	char *keeped;
+	char *tmp;
 	char *buf;
+	char *after_n;
 
+	tmp = NULL;
 	if (fd < 0 || line == NULL)
 		return (-1);
-	buf = read_the_buffer(fd);
+	buf = read_the_buffer(fd, &after_n);
 	if (BUFF_SIZE != ft_strlen(buf))
-	keeped = str_to_keep ();
-	*line = ft_strjoin_free(keeped, buf)
+		tmp = ft_strdup(str_to_keep (after_n));
+	*line = ft_strjoin_free(tmp, buf);
+	return (0);
 }
