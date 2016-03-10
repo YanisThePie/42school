743ca7f67614e9d62eba81cad6710f31b77ba221
user <user@MacBook-Pro-de-user-3.local>
2016-03-10 02:42:11 +0100

    commit

--- GNL2/get_next_line.c
+++ GNL2/get_next_line.c
@@ -37,53 +37,61 @@ char *str_to_keep (char *after_n)
 			size = how_long_for (str_to_keep, '\n');
 			to_send = ft_strsub(str_to_keep, 0, size); // if \n in str_to_keep, send until it, and keep the rest.
 			str_to_keep = ft_strdup(ft_strchr(str_to_keep, '\n') + 1);
+			free(to_send);
 			return (to_send);
 		}
 		else
 		{
 			to_keep = ft_strdup(str_to_keep);
 			ft_bzero(str_to_keep, ft_strlen(str_to_keep)); // moyen de ne pas ecrire par dessus sans tenter de l'effacer?
-			return (str_to_keep); //else return all.
+			return (to_keep); //else return all.
 		}
 	}
-	str_to_keep = ft_strnew (0);
-	str_to_keep = ft_strdup (after_n);
-	return (str_to_keep);
+	else
+	{
+		//str_to_keep = ft_strnew (0); // strdup alloue deja?
+		str_to_keep = ft_strdup (after_n);
+		return (str_to_keep);
+	}
 }
 
 char *read_the_buffer(int fd, char **after_n)
 {
 	char *buf;
 	char *buf_joined;
+	int size;
 
 	buf = ft_strnew(BUFF_SIZE);
 	while (read(fd, buf, BUFF_SIZE) > 0)
 	{
-		if (ft_strchr(buf, '\n'))// if we found a \n, send all the rest to gnl and stop the read.
+		//ft_putstr(buf);
+		if (ft_strchr(buf_joined, '\n'))// if we found a \n, send all the rest to gnl and stop the read.
 		{
-			*after_n = ft_strdup(strchr(buf, '\n') + 1);
-			return (buf);
+			*after_n = ft_strdup(strchr(buf_joined, '\n') + 1);
+			size = how_long_for (buf, '\n');
+			buf_joined = ft_strjoin_free(buf_joined, ft_strsub(buf, 0, size));
+			return (buf_joined);
 		}
 		else
 		{
 			buf_joined = (buf_joined == NULL) ? ft_strnew(0) : buf_joined;
 			buf_joined = ft_strjoin(buf_joined, buf);// we join buffers, or we create one if doesnt exist.
+			//ft_putstr(buf_joined);
 		}
 	}
-	return (buf);// If Read is over, return the buffer.
+	return (buf_joined);// If Read is over, return the buffer.
 }
 int get_next_line(int const fd, char **line)
 {
 	char *tmp;
-	char *buf;
+	char *buffer;
 	char *after_n;
 
 	tmp = NULL;
 	if (fd < 0 || line == NULL)
 		return (-1);
-	buf = read_the_buffer(fd, &after_n);
-	if (BUFF_SIZE != ft_strlen(buf))
-		tmp = ft_strdup(str_to_keep (after_n));
-	*line = ft_strjoin_free(tmp, buf);
+	buffer = read_the_buffer(fd, &after_n);
+	tmp = ft_strdup(str_to_keep (after_n));
+	*line = ft_strjoin_free(tmp, buffer);
 	return (0);
 }
--- GNL2/to_del/main.c
+++ GNL2/to_del/main.c
@@ -13,14 +13,6 @@ int main (int argc, char **argv)
 		send = NULL;
 		fd = open(argv[1], O_RDONLY);
 
-		while((this = get_next_line(fd, &send)))
-		{
-			ft_putnbr(this);
-			ft_putchar('\n');
-			ft_putstr(send);
-			free(send);
-			send = NULL;
-			i++;
-		}
-		ft_putnbr(this);
+		this = get_next_line(fd, &send);
+		ft_putstr(send);
 }
