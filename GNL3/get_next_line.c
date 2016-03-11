eb3615c3bfc73b0757c9b217381be9d50a017654
Yanis ISMAIL <yismail@e1r5p19.42.fr>
2016-03-11 05:49:23 +0100

    almosy

--- GNL3/get_next_line.c
+++ GNL3/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/10 22:12:15 by yismail           #+#    #+#             */
-/*   Updated: 2016/03/11 04:55:25 by yismail          ###   ########.fr       */
+/*   Updated: 2016/03/11 05:48:33 by yismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int ft_read (char **buffer, char **tmp, int fd)
 			return (1);
 		}
 	}
-	if ((ret == 0 && *tmp == NULL && !*buf) || (ret == 0 && !*buf))
+	if (((ret == 0 && *tmp == NULL && !*buf)) || (ret == 0 && !*buf))
 		return (0);
 	if (ret == -1)
 		return (ret);
@@ -64,12 +64,6 @@ int get_next_line(int const fd, char **line)
 		free(cpy);
 	}
 	ret = ft_read(&buffer, &tmp, fd);
-	//ft_putendl("buffer_final");
-	//ft_putendl(buffer);
 	*line = ft_strdup(buffer);
-	//ft_putendl(*line);
-	//ft_putstr("this is ret");
-	//ft_putnbr(ret);
-	//ft_putchar('\n');
 	return (ret);
 } 
--- GNL3/to_del/main.c
+++ GNL3/to_del/main.c
@@ -7,10 +7,13 @@ int main (int argc, char **argv)
 	(void)argc;
 	char *line;
 	int fd = 0;
+	int ret;
 	
-	while(get_next_line(fd, &line) == 1)
+	while((ret = get_next_line(fd, &line)) == 1)
 	{
 		printf("%s\n", line);
+        ft_putnbr(ret);
+        ft_putchar('\n');
 		ft_strdel(&line);
 	}
 	return (0);
