2c5814db2dc02244509428101afe94365b2e546c
Yanis ISMAIL <yismail@e2r11p17.42.fr>
2016-03-16 12:38:18 +0100

    FINI

--- GNL2/get_next_line.h
+++
@@ -1,8 +0,0 @@
-#include "./libft/libft.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#define BUFF_SIZE 3
-
-int get_next_line (int const fd, char **line);
