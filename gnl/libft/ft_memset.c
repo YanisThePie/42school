a26780670dfbb0750661babd358bf7dda5a6736f
Yanis ISMAIL <yismail@e1r5p19.42.fr>
2016-06-27 07:08:18 +0200

    VOILAA

---
+++ gnl/libft/ft_memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismail <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/28 17:32:05 by yismail           #+#    #+#             */
+/*   Updated: 2015/12/10 19:17:44 by yismail          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	i = 0;
+	str = (unsigned char *)b;
+	while (len > 0)
+	{
+		str[i] = (unsigned char)c;
+		i++;
+		len--;
+	}
+	return (b);
+}
